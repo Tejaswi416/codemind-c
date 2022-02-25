7c999ca43459783fd7c6982ba831177a1a56934b
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-02-25 20:42:20 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n;j>0;j--)         {             printf("%d ",j);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,j;
-    scanf("%d",&a);
-    for(i=1;i<=a;i++)
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=a;j++)
+        for(j=n;j>0;j--)
         {
-            if(i==j)
-            printf("0");
-            else
-            printf("x");
+            printf("%d ",j);
         }
         printf("
 ");
-    }return 0;
+    }
+    return 0;
 }
