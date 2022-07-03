765715090367c8bc9ebcb7c3544fc3eef429a062
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-03 19:15:19 +0530

    #include<stdio.h> #include<math.h> int main() {     int m,n,i,j,count=0;     scanf("%d %d",&m,&n);     if(m==1)     {         m=2;     }     for(j=m;j<=n;j++)     {         count=0;         for(i=2;i<=sqrt(j);i++)         {             if(j%i==0)             {                 count++;             }         }         if(count==0)         {             printf("%d ",j);         }     }     return 0; }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int m,n,i,j,count=0;
+    scanf("%d %d",&m,&n);
+    if(m==1)
+    {
+        m=2;
+    }
+    for(j=m;j<=n;j++)
+    {
+        count=0;
+        for(i=2;i<=sqrt(j);i++)
+        {
+            if(j%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==0)
+        {
+            printf("%d
+",j);
+        }
+    }
+    return 0;
+}
