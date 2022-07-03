9e7b94acf460bc47b8d5adf7f6c38a82cd36ec3f
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-03 18:14:03 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     //int l_index=arr[0];     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==0)         {             printf("%d",i);             break;         }     }
        return 0;
    }

---
+++ Last_even_element_index.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    //int l_index=arr[0];
+    for(i=n-1;i>=0;i--)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+    
+    return 0;
+}
