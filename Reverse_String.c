027c962f3bbc46ee362b6f53750535eb37c4ef30
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-11 11:58:10 +0530

    #include<stdio.h> #include<string.h> int main() {     int i,t;     char s[20];     scanf("%[^ ]s",s);     t=strlen(s);     for(i=t-1;i>=0;i--)     printf("%c",s[i]);     return 0; }

---
+++ Reverse_String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,t;
+    char s[20];
+    scanf("%[^
+]s",s);
+    t=strlen(s);
+    for(i=t-1;i>=0;i--)
+    printf("%c",s[i]);
+    return 0;
+}
