7110aa0b4931d287eca95b292dd674e9e76df6a9
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-11 18:12:00 +0530

    #include <stdio.h> #include<math.h> int main() {     int n;     scanf("%d", &n);     int a[n], b=0, i,j=0;     for (i = 0; i < n; i++)         scanf("%d", &a[i]);     for(i=0;i<n/2+1;i++)     {         if(i<n/2)         b+=a[i];         // j+=a[n/2+i];     }     for(i=n/2;i<n;i++)     j+=a[i];     printf("%d ",b);     printf("%d",j);     return 0; }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n], b=0, i,j=0;
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for(i=0;i<n/2+1;i++)
+    {
+        if(i<n/2)
+        b+=a[i];
+        // j+=a[n/2+i];
+    }
+    for(i=n/2;i<n;i++)
+    j+=a[i];
+    printf("%d
+",b);
+    printf("%d",j);
+    return 0;
+}
