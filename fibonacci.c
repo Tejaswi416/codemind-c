5e66a698e3f2c3d28ee9d6fe4ced8c9f629199fe
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-13 21:11:36 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     int a=0,b=0,c=1;     printf("0 1 ");     scanf("%d",&i);     for(i=0;i<n-2;i++)     {         a=b+c;         b=c;         c=a;         printf("%d ",a);     }     return 0; }

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a=0,b=1,c;
+    int i,j,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int a=0,b=0,c=1;
+    printf("0 1 ");
+    scanf("%d",&i);
+    for(i=0;i<n-2;i++)
     {
-        printf("%d ",a);
-        c=a+b;
-        a=b;
+        a=b+c;
         b=c;
+        c=a;
+        printf("%d ",a);
     }
     return 0;
 }
