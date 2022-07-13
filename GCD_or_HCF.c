7a99439495fd78fd6a286e1ac8ef74f431d19527
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-13 21:07:11 +0530

    #include<stdio.h> int main() {     int a,b,t,i=0;     scanf("%d%d",&a,&b);     for(t=2;t<=a&&t<=b;t++)     {         if(a%t==0&&b%t==0)         {i=t;}     }     if(i!=0)     printf("%d",i);     else     printf("1");     return 0; }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,min;
+    int a,b,t,i=0;
     scanf("%d%d",&a,&b);
-    if(a<b)
-    min=a;
-    else
-    min=b;
-    while(1)
+    for(t=2;t<=a&&t<=b;t++)
     {
-        if(a%min==0&&b%min==0)
-        {
-            printf("%d",min);
-            break;
-        }
-        min--;
+        if(a%t==0&&b%t==0)
+        {i=t;}
     }
+    if(i!=0)
+    printf("%d",i);
+    else
+    printf("1");
     return 0;
 }
