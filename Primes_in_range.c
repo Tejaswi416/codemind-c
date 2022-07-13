6759e238ef756e0d58c6ca20a5dc302f2f047ab8
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-13 21:25:18 +0530

    #include<stdio.h> #include<math.h> int isprime(int i) {     if(i==1)     return 0;     if(i==2)     return 1;     int j; for(j=2;j<=sqrt(i);j++) { if(i%j==0) {   return 0; } } return 1;
    }
    int main()
    {
    int n1,n2,i,j,c=0;
    scanf("%d",&n1);
    scanf("%d",&n2);
    for(i=n1;i<=n2;i++)
    {
      if(isprime(i)){
       c++;
     }
    }
    printf("%d",c);
    return 0;
    }

--- Primes_in_range.c
+++ Primes_in_range.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<math.h>
-int isprime(int i){
-    if (i==1)
+int isprime(int i)
+{
+    if(i==1)
     return 0;
     if(i==2)
     return 1;
     int j;
-    for(j=2;j<=sqrt(i);j++)
-    {
-        if(i%j==0)
-        {
-            return 0;
-        }
-    }
-    
-    return 1;
-  
+for(j=2;j<=sqrt(i);j++)
+{
+if(i%j==0)
+{
+  return 0;
+}
+}
+return 1;
+
 }
 int main()
 {
-    int n1,n2,i,j,c=0;
-    scanf("%d",&n1);
-    scanf("%d",&n2);
-    for(i=n1;i<=n2;i++)
-    {
-        if(isprime(i)){
-            c++;
-        }
-    }
-    printf("%d",c);
-    return 0;
+int n1,n2,i,j,c=0;
+scanf("%d",&n1);
+scanf("%d",&n2);
+for(i=n1;i<=n2;i++)
+{
+  if(isprime(i)){
+   c++;
+ }
 }
- 
+printf("%d",c);
+return 0;
+}
